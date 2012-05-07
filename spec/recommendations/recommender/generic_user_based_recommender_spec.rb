48a5986e1464c001411f05965c575912a68fdd2b
Carlo <carlo.scarioni@gmail.com>
2012-05-07 11:59:59 +0100

    Fixed spec

--- spec/recommendations/recommender/generic_user_based_recommender_spec.rb
+++ spec/recommendations/recommender/generic_user_based_recommender_spec.rb
@@ -11,10 +11,10 @@ describe Recommendations::Recommender::GenericUserBasedRecommender do
     neighborhood.should_receive(:user_neighborhood).with(user).and_return([2, 3, 4])
     data_model.should_receive(:items_for_users).with([2, 3, 4]).and_return([1, 2, 3, 4, 5, 6])
     data_model.should_receive(:items_for_users).with([1]).and_return([1, 2])
-    rating_estimator.should_receive(:estimate).with(user, neighborhood, 3).and_return(3.0)
-    rating_estimator.should_receive(:estimate).with(user, neighborhood, 4).and_return(4.0)
-    rating_estimator.should_receive(:estimate).with(user, neighborhood, 5).and_return(2.0)
-    rating_estimator.should_receive(:estimate).with(user, neighborhood, 6).and_return(3.5)
+    rating_estimator.should_receive(:estimate).with(user, [2, 3, 4], 3).and_return(3.0)
+    rating_estimator.should_receive(:estimate).with(user, [2, 3, 4], 4).and_return(4.0)
+    rating_estimator.should_receive(:estimate).with(user, [2, 3, 4], 5).and_return(2.0)
+    rating_estimator.should_receive(:estimate).with(user, [2, 3, 4], 6).and_return(3.5)
     recommendations = recommender.recommend(user, 3)
     recommendations.size.should == 3
     recommendations[0].item.should == 4
